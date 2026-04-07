211cf26a26bc0d06f9084ef7b70c9e3ecf73b6bb
Gian-Catrina <gian.catrina@gmx.ch>
2026-04-07 11:32:33 +0200

    Update

--- check.sh
+++ check.sh
@@ -5,9 +5,19 @@
 # Letztes Aenderungensdatum: 28.04.2026
 # Beschreibung: Dieses Script, ueberprueft ob Nmap installiert wurde, und somit auch, ob das Playbook erfolgreich durchgelaufen ist.
 
-(crontab -l 2>/dev/null; echo "0 12 * * * /bin/bash /pfad/zu/deinem/check_nmap.sh") | crontab -
+echo "========================================================================"
+echo "Gib den Absoluten Pfad zum check.sh an. Wenn du es runtergeladen hast, ist es wahrscheinlich /home/'deinusername'/M122_Projektarbeit/check.sh"
+echo "========================================================================"
+read checkpfad
 
-LOG="playbook.log"
+(crontab -l 2>/dev/null; echo "0 12 * * * /bin/bash $checkpfad") | crontab -
+
+echo "========================================================================"
+echo "Gib den Absoluten Pfad zum Logfile an, genau den gleichen wie oben, aber anstatt /check.sh einfach /playbook.log"
+echo "========================================================================"
+read logpfad
+
+LOG="$logpfad"
 
 if command -v nmap > /dev/null; then
     echo "Installation hat geklappt"
