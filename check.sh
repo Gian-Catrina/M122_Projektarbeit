8cf7ab0cec4389c2fc073465ac41c7d1dd6e07ee
Gian-Catrina <gian.catrina@gmx.ch>
2026-04-14 08:40:22 +0200

    final update

--- check.sh
+++ check.sh
@@ -10,19 +10,17 @@ echo "Gib den Absoluten Pfad zum check.sh an. Wenn du es runtergeladen hast, ist
 echo "========================================================================"
 read checkpfad
 
-(crontab -l 2>/dev/null; echo "0 12 * * * /bin/bash $checkpfad") | crontab -
+(crontab -l 2>/dev/null; echo "0 12 * * * /bin/bash \"$checkpfad\"") | crontab -
 
 echo "========================================================================"
 echo "Gib den Absoluten Pfad zum Logfile an, genau den gleichen wie oben, aber anstatt /check.sh einfach /playbook.log"
 echo "========================================================================"
 read logpfad
 
-LOG="$logpfad"
-
 if command -v nmap > /dev/null; then
     echo "Installation hat geklappt"
-    echo "geklappt" > $LOG
+    echo "geklappt" > $logpfad
 else
     echo "Hat nicht geklappt"
-    echo "nicht geklappt" > $LOG
+    echo "nicht geklappt" > $logpfad
 fi
